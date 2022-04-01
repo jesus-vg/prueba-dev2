e933d445926173ced39376e43ba151770c9ab017
Jesus Villegas <jesus777.jsgl@gmail.com>
2022-04-01 13:07:12 -0600

    vue agregado: para registrar componentes y montarlos en el DOM

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,28 @@
-require('./bootstrap');
+require("./bootstrap");
+
+import Alpine from "alpinejs";
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+// aqui importamos nuestras propias librerias
+import { createApp } from "vue";
+
+const app = createApp({
+    /* root component options */
+});
+
+// aqui importamos nuestros componentes
+app.component(
+    "eliminar-vendedor",
+    require("./components/vendedor/EliminarVendedor.vue").default
+);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+app.mount("#app");
