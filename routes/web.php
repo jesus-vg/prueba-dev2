5b8792124b5fe75ff62cf3b26266c42888d3306e
Jesus Villegas <jesus777.jsgl@gmail.com>
2022-04-01 13:08:09 -0600

    rutas para vendedores.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VendedorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -11,8 +12,17 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get( '/', function () {
+    return view( 'welcome' );
+} );
+
+Route::middleware( ['auth'] )->group( function () {
+
+    Route::resource( 'vendedores', VendedorController::class )->parameters( [
+        'vendedore' => 'vendedor', // nombre del parametro que se usa en las rutas ej. /vacantes/{vacante} (vacante es el parametro)
+    ] );
+} );
+
+require __DIR__ . '/auth.php';
