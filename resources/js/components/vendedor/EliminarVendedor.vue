dcb256ba8dd208b280ea854a3e993dd6f67d3d60
Jesus Villegas <jesus777.jsgl@gmail.com>
2022-04-01 13:06:26 -0600

    componente vue aliminar vendedor

---
+++ resources/js/components/vendedor/EliminarVendedor.vue
@@ -0,0 +1,20 @@
+<template>
+    <button class="px2 py-2" @click="eliminarVendedor()">Eliminar</button>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: Number,
+            required: true,
+        },
+        nombre: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
+
+<style></style>
